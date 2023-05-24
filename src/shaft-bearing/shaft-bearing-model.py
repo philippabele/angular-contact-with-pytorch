ec6d5e4b4cbbe013e3e3520c2a36c3ea362fc0e1
Jan Fröhlich <jan.froehlich.janf@gmail.com>
2023-05-25 00:25:14 +0200

    feat: integrate TensorBoard

--- src/shaft-bearing/shaft-bearing-model.py
+++ src/shaft-bearing/shaft-bearing-model.py
@@ -1,22 +1,24 @@
+from matplotlib import pyplot as plt
+import numpy as np
 import torch
 import torch.nn as nn
 import pandas as pd
 from torch.utils.data import Dataset, DataLoader
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-
+from torch.utils.tensorboard import SummaryWriter
 
 # Hyperparameters
-learning_rate = 1e-2
+learning_rate = 1e-5
 batch_size = 64
-epochs = 100
+epochs = 10000
 test_size = 0.01
+log_count = 10
 
-load_model = True
+load_model = False
 save_model = True
 train_model = True
 eval_model = True
-
 
 # Define custom dataset
 class ShaftBearingDataset(Dataset):
@@ -38,10 +40,15 @@ y = data['Lifetime'].values
 # Split into train and test set
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=42)
 
-# Standardize the data
-scaler = StandardScaler()
-X_train = scaler.fit_transform(X_train)
-X_test = scaler.transform(X_test)
+# Standardize the features
+X_scaler = StandardScaler()
+X_train = X_scaler.fit_transform(X_train)
+X_test = X_scaler.transform(X_test)
+
+# Standardize the labels
+y_scaler = StandardScaler()
+y_train = y_scaler.fit_transform(y_train.reshape(-1,1)).flatten()
+y_test = y_scaler.transform(y_test.reshape(-1,1)).flatten()
 
 # Create datasets
 train_data = ShaftBearingDataset(X_train, y_train)
@@ -49,9 +56,6 @@ test_data = ShaftBearingDataset(X_test, y_test)
 
 print(f'Train data: {len(train_data)} samples')
 print(f'Test data: {len(test_data)} samples')
-
-# for i in range(len(test_data)):
-#     print(test_data[i])
 
 # Create dataloaders
 dataloader = DataLoader(train_data, batch_size=32, shuffle=True)
@@ -85,11 +89,15 @@ if load_model:
     except:
         print("No model found, training from scratch.")
 
-
-
 # Define loss and optimizer
 criterion = nn.MSELoss()
 optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+
+# Set up TensorBoard writer
+writer = SummaryWriter('runs/ShaftBearing')
+writer_no_outliers = SummaryWriter('runs/ShaftBearing_no_outliers')
+
+loss_values = []
 
 # Training loop
 def train(dataloader, model, criterion, optimizer, epochs):
@@ -99,15 +107,22 @@ def train(dataloader, model, criterion, optimizer, epochs):
             outputs = model(inputs)
             loss = criterion(outputs, labels)
 
-            # print inputs, labels and outputs
-            if (epoch+1) % 10 == 0 and i == 0:  # print for the first batch of every 10th epoch
-                print("Inputs: ", inputs)
-                print("Labels: ", labels)
-                print("Outputs: ", outputs)
-
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
+
+        loss_values.append(loss.item())
+        writer.add_scalar('train/loss/all', loss.item(), epoch)
+        if (epoch+1) % log_count == 0:
+            print(f'Epoch {epoch+1}/{epochs}, Loss: {loss.item()}')
+
+    # If loss_value is not mor than 2 std away from the mean, add it to the no_outliers loss
+    mean_loss = np.mean(loss_values)
+    std_loss = np.std(loss_values)
+    for i, loss_value in enumerate(loss_values):
+        if np.abs(loss_value - mean_loss) <= 1*std_loss:
+            print(loss_value)
+            writer_no_outliers.add_scalar('train/loss/no_outliers', loss_value, i)
 
 
 def eval(dataloader, model, criterion):
@@ -121,6 +136,8 @@ def eval(dataloader, model, criterion):
 
 if train_model:
     train(dataloader, model, criterion, optimizer, epochs)
+    writer.close()
+    writer_no_outliers.close()
     if save_model:
         torch.save(model.state_dict(), 'data/ShaftBearing/model.pth')
         print('Saved model state as data/ShaftBearing/model.pth.')
