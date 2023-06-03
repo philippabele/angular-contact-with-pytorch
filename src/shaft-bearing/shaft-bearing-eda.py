da3e4985f215ea4666376f7fe061e488f87dc39b
Jan Fröhlich <jan.froehlich.janf@gmail.com>
2023-06-03 18:03:18 +0200

    feat: remove unnecessary features from EDA

--- src/shaft-bearing/shaft-bearing-eda.py
+++ src/shaft-bearing/shaft-bearing-eda.py
@@ -6,11 +6,6 @@ import numpy as np
 # Load the data
 data = pd.read_csv('data/ShaftBearing/data.csv')
 
-# Create interaction features
-data['Fr*n'] = data['Fr']*data['n']
-data['Fr/n'] = data['Fr']/data['n']
-data['LifetimeFunc'] = 4.13786 * 10**17 * (data['Fr']) ** (-10/3) * (data['n']) ** (-1.0)
-
 # Basic information about the data
 print('\nFirst few rows of the data:')
 print(data.head())
@@ -22,36 +17,35 @@ print('\nMissing Values:')
 print(data.isnull().sum())
 
 # Distribution Plots (Logarithmic Scale for Lifetime)
-fig1, ax1 = plt.subplots(2, 3, figsize=(15, 10))
+fig1, ax1 = plt.subplots(1, 3, figsize=(15, 5))
 for i, column in enumerate(data.columns):
     if column == 'Lifetime' or column == 'Fr*n' or column == 'Fr/n' or column == 'LifetimeFunc':
         # Create a temporary variable for Lifetime, replacing 0 with a very small number
         temp_column = data[column].replace(0, 0.0001)
-        sns.histplot(np.log(temp_column), ax=ax1[i//3, i%3])
-        ax1[i//3, i%3].set_title('Logarithmic Scale for ' + column)
+        sns.histplot(np.log(temp_column), ax=ax1[i%3])
+        ax1[i%3].set_title('Logarithmic Scale for ' + column)
     else:
         n_bins = data[column].nunique()
-        sns.histplot(data[column], bins=n_bins, ax=ax1[i//3, i%3])
-        ax1[i//3, i%3].set_title('Distribution of ' + column)
+        sns.histplot(data[column], bins=n_bins, ax=ax1[i%3])
+        ax1[i%3].set_title('Distribution of ' + column)
 fig1.suptitle('Distribution Plots')
 fig1.savefig('docs/assets/bearings-eda/distribution-plots.png')
 
 # Boxplots
-fig4, ax4 = plt.subplots(2, 3, figsize=(15, 10))
+fig4, ax4 = plt.subplots(1, 3, figsize=(15, 5))
 for i, column in enumerate(data.columns):
-    sns.boxplot(x=data[column], ax=ax4[i//3, i%3])
-    ax4[i//3, i%3].set_title('Boxplot of ' + column)
+    sns.boxplot(x=data[column], ax=ax4[i%3])
+    ax4[i%3].set_title('Boxplot of ' + column)
 fig4.suptitle('Boxplots')
 fig4.savefig('docs/assets/bearings-eda/boxplots.png')
 
 # Pairplot
 data_pair = data.copy()
 data_pair['Lifetime_log'] = np.log(data_pair['Lifetime'])
-with sns.plotting_context("notebook", font_scale=1.5):
-    pair = sns.pairplot(data_pair, hue='Lifetime_log', palette='crest')
-    plt.subplots_adjust(top=0.95)
-    plt.suptitle('Pairplot of Fr, n, Lifetime and interaction features')
-    pair.savefig('docs/assets/bearings-eda/pairplot.png')
+pair = sns.pairplot(data_pair, hue='Lifetime_log', palette='crest')
+plt.subplots_adjust(top=0.95)
+plt.suptitle('Pairplot of Fr, n, Lifetime and interaction features')
+pair.savefig('docs/assets/bearings-eda/pairplot.png')
 
 # Heatmap of Correlations
 corr = data.corr()
@@ -73,20 +67,20 @@ ax3.set_zlabel('Lifetime')
 plt.title('3D Plot of Fr, n and Lifetime')
 fig3.savefig('docs/assets/bearings-eda/3dplot.png')
 
-import matplotlib.pyplot as plt
-
-# 3D Plot of Residuals
+# Estimate Lifetime Function and calculate Residuals
+data['LifetimeFunc'] = 4.13786 * 10**17 * (data['Fr']) ** (-10/3) * (data['n']) ** (-1.0)
 data['Residuals (% -1)'] = (data['Lifetime'] / data['LifetimeFunc']) - 1
 print('\nResiduals:')
 print(data['Residuals (% -1)'].describe())
+
+# 3D Plot of Residuals
 fig32 = plt.figure()
 ax32 = fig32.add_subplot(111, projection = '3d')
-ax32.scatter(data['Fr'], data['n'], data['Residuals (% -1)'] / 10**20)
+ax32.scatter(data['Fr'], data['n'], data['Residuals (% -1)'])
 ax32.set_xlabel('Fr')
 ax32.set_ylabel('n')
 ax32.set_zlabel('Residuals')
 plt.title('3D Plot of Fr, n and Residuals')
 fig32.savefig('docs/assets/bearings-eda/3dplot-residuals.png')
-
 
 plt.show()
