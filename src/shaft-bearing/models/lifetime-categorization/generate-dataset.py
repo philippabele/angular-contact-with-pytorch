826876a05f94b4d5d0eac1b4767dacba028f3b68
Jan Fröhlich <jan.froehlich.janf@gmail.com>
2023-06-08 23:08:38 +0200

    feat: script for generating bearing datasets

---
+++ src/shaft-bearing/models/lifetime-categorization/generate-dataset.py
@@ -0,0 +1,31 @@
+import numpy as np
+import pandas as pd
+import os
+from config import config
+
+# Modify the ranges, intervals, and output path as needed
+fr_range = (1, 100)
+n_range = (1000, 10000)
+fr_interval = 2
+n_interval = 100
+output_path = os.path.join(config["folder_path"], 'data-gen-lt.csv')
+
+def generate_lifetime(fr, n):
+    return 4.13786 * 10**17 * fr ** (-10/3) * n ** (-1.0)
+
+def generate_dataset(fr_range, n_range, fr_interval, n_interval, output_path):
+    fr_values = np.arange(fr_range[0], fr_range[1], fr_interval)
+    n_values = np.arange(n_range[0], n_range[1], n_interval)
+
+    data = []
+    for fr in fr_values:
+        for n in n_values:
+            lifetime = generate_lifetime(fr, n)
+            data.append([fr, n, lifetime])
+    
+    # Save data to a CSV file
+    df = pd.DataFrame(data, columns=['Fr', 'n', 'Lifetime'])
+    df.to_csv(output_path, index=False)
+    print(f'Dataset generated and saved to {output_path}')
+
+generate_dataset(fr_range, n_range, fr_interval, n_interval, output_path)
